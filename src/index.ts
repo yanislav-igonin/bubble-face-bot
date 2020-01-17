f22ef5753a58ae38a8a34fd091208ff3f318641b
Yanislav Igonin <jan.oasis@gmail.com>
2020-01-17 14:57:44 +0300

    Add some code

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,52 @@
-import Telegraf, { ContextMessageUpdate } from 'telegraf';
+import Telegraf, { ContextMessageUpdate, Telegram } from 'telegraf';
 import * as ngrok from 'ngrok';
 import * as crypto from 'crypto';
+import axios from 'axios';
+import FormData from 'form-data';
 
 import { app } from './config';
 import { logger } from './modules';
 
 const bot = new Telegraf(app.botToken);
+const telegram = new Telegram(app.botToken);
 
 bot.catch((err: Error): void => {
   logger.error(`ERROR: ${err}\n`);
 });
 
 bot.start((ctx: ContextMessageUpdate): void => {
-  ctx.reply(`${new Date().toLocaleString()} - start`);
+  ctx.reply(
+    'Превратите себя в героя комиксов с помощью технологии машинного обучения'
+    + ' Bubble Face. Каждый рисунок создается на основе уникальных стилей'
+    + 'художников BUBBLE. Станьте частью нашей вселенной и удивите ваших друзей!',
+  );
 });
 
-bot.on('text', (ctx: ContextMessageUpdate): void => {
-  const message = ctx.update.message ? ctx.update.message.text : '';
-  ctx.reply(`${new Date().toLocaleString()} - ${message}`);
+bot.on('photo', async (ctx: ContextMessageUpdate): Promise<void> => {
+  try {
+    // @ts-ignore
+    const { photo } = ctx.update.message;
+    const fileId = photo[photo.length - 1].file_id;
+    // @ts-ignore // 'getFile' does not exist, bad typings
+    // const fileInfo = await telegram.getFile(fileId);
+    // console.log('DEBUG: fileInfo', fileInfo);
+    const fileLink = await telegram.getFileLink(fileId);
+    const { data: imageStream } = await axios.get(fileLink, {
+      responseType: 'stream',
+    });
+    console.log('DEBUG: imageStream', imageStream);
+    const formData = new FormData();
+    formData.append('image', imageStream);
+    const { data: processedImageStream } = await axios.post(
+      'https://face.bubble.ru/_api/face',
+      formData,
+      { responseType: 'stream' },
+    );
+    console.log('DEBUG: processedImageStream', processedImageStream);
+    ctx.replyWithPhoto(processedImageStream);
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
 const launch = async (): Promise<void> => {
